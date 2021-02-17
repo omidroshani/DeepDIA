2c66325e37093d8a398734c411993f4138698b21
MMoghaddam86 <73430391+MMoghaddam86@users.noreply.github.com>
2021-02-17 09:37:02 +0330

    Update predict.py

--- code/deepms2/py/predict.py
+++ code/deepms2/py/predict.py
@@ -12,32 +12,29 @@ from pepms2.utils import save_data_json, load_peptides_csv
 options = PeptideMS2Options.default()
 
 predict_dir = '.'
-
-model_path = [ 
-    os.path.join(predict_dir, 'models', f) 
-    for f in os.listdir(os.path.join(predict_dir, 'models'))
-    if re.match(r'^epoch_[0-9]+\.hdf5$', f) is not None
-][-1]
+for f in os.listdir(os.path.join(predict_dir,'charge')):
+  if re.match(r'^epoch_[0-9]+\.hdf5$', f) is not None:
+      model_path =os.path.join(predict_dir, 'charge', f) 
 
 predictor = PeptideMS2Predictor(options)
 predictor.model = build_model_from_weights(options=options, weights_path=model_path)
 
 data_files = [
-    os.path.join(predict_dir, f) 
-    for f in os.listdir(predict_dir) 
+    os.path.join(predict_dir,'charge', f) 
+    for f in os.listdir(os.path.join(predict_dir,'charge')) 
     if f.endswith('.peptide.csv')
 ]
 
 for file in data_files:
+    
     peptides = load_peptides_csv(file)
     sequences = peptides['sequence']
-    modifications = peptides['modification']
+    modifications = peptides['modification'] 
     
     prediction = predictor.predict(sequences, modifications)
 
     save_data_json(
         data=prediction,
         file=re.sub(r'\.peptide\.csv$', '.prediction.ions.json', file)
+        #file=('prediction.ions.json')
     )
-
-        
