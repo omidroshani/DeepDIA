69c2696db2dfeee82efaeb6a0fbc8269f2ba6eb3
MMoghaddam86 <73430391+MMoghaddam86@users.noreply.github.com>
2021-02-17 09:38:41 +0330

    Update train.py

--- code/deepms2/py/train.py
+++ code/deepms2/py/train.py
@@ -8,22 +8,22 @@ from pepms2.utils import load_data_dir, save_data_json #, filter_data
 
 options = PeptideMS2Options.default()
 
-train_dir = '.'
+train_dir = './charge/'
 data, data_files = load_data_dir(train_dir)
 # data, filter_indexs = filter_data(data, max_sequence_length=options.max_sequence_length, threshold=0.1)
 
-os.makedirs(os.path.join(train_dir, 'models'), exist_ok=True)
+
+os.makedirs(os.path.join(train_dir, 'models1'), exist_ok=True)
 
 trainer = PeptideMS2Trainer(
     options=options,
-    save_path=os.path.join(train_dir, 'models', 'epoch_{epoch:03d}.hdf5'),
+    save_path=os.path.join(train_dir, 'models1', 'epoch_{epoch:03d}.hdf5'),
     log_path=os.path.join(train_dir, 'training.log')
 )
 result = trainer.train(data)
 result['files'] = data_files
 # result['filter'] = filter_indexs
 
-trainer.save_model(os.path.join(train_dir, 'models', 'last_epoch.hdf5'))
+trainer.save_model(os.path.join(train_dir, 'models1', 'last_epoch.hdf5'))
 
 save_data_json(result, os.path.join(train_dir, 'training.json'))
-
